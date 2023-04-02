9375d6b1c799e7572dc370eb83a6ec49a35e0d2c
Letícia Vitória <99629704+leticiazooe@users.noreply.github.com>
2023-04-02 14:21:16 -0300

    Create Venda.cs

---
+++ Venda.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgramacaoWeb_CSharp_Fatec.Classes
+{
+    public class Venda
+    {
+      
+        private int IdVenda;
+        private DateTime DataVenda;
+        private List<Produto> ListaProdutos;
+
+        
+        public Venda(int idVenda, DateTime dataVenda, List<Produto> listaProdutos)
+        {
+            IdVenda = idVenda;
+            DataVenda = dataVenda;
+            ListaProdutos = listaProdutos;
+        }
+
+        public void getDadosVenda()
+        {
+            Console.WriteLine("\nID da venda: " + IdVenda);
+            Console.WriteLine("Data da venda: " + DataVenda.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Lista de Produtos: " );
+
+            foreach (var item in ListaProdutos)
+            {
+                Console.WriteLine("- Produto: " + item.ExibeDados());
+            }
+        }
+    }
+}
